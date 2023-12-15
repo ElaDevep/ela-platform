38949182b2047f1b8f1714de1f8331a0898dfb16
felipe <fepinzon77@gmail.com>
2023-12-15 13:22:40 -0500

    [ADD] Modules but are not complete

--- src/constants.tsx
+++ src/constants.tsx
@@ -11,21 +11,21 @@ export const SIDENAV_ITEMS: SideNavItem[] = [
   {
     title: 'Agua',
     path: '/Water',
-    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+    icon: <Icon icon="lucide:droplets" width="24" height="24" />,
   },
   {
     title: 'Energia',
     path: '/Energy',
-    icon: <Icon icon="lucide:mail" width="24" height="24" />,
+    icon: <Icon icon="lucide:plug-zap" width="24" height="24" />,
   },
   {
     title: 'Residuos',
     path: '/Waste',
-    icon: <Icon icon="lucide:settings" width="24" height="24" />,
+    icon: <Icon icon="lucide:rotate-3d" width="24" height="24" />,
   },
   {
     title: 'Educación',
     path: '/Education',
-    icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+    icon: <Icon icon="lucide:graduation-cap" width="24" height="24" />,
   },
 ];
