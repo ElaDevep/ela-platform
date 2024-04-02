9fee53a50b05eaa16bd08b044ec427fd660c220f
santher <erickhernandop@gmail.com>
2024-04-02 16:41:17 -0500

    Configuracion de Middleware para github pages

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
+const githubPath='/ela-platform'
+
 export function middleware(request: NextRequest) {
     const currentUser = request.cookies.get('currentUser')?.value
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL(githubPath+'/login', request.url))
 }
 
 export const config = {
-    matcher: ['/'],
+    matcher: [githubPath],
 }
