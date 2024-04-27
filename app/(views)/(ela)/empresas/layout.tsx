74b7f4fdb5b5c2d4f9615fe7b6441d1f6a4d22c6
santher <erickhernandop@gmail.com>
2024-04-26 22:07:56 -0500

    Correccion de Layouts2

--- app/(views)/(ela)/empresas/layout.tsx
+++ app/(views)/(ela)/empresas/layout.tsx
@@ -3,13 +3,11 @@ import styles from "./layout.module.sass"
 
 export default function Layout({  
     children,
-    form
     }: Readonly<{
     children: React.ReactNode;
-    form: React.ReactNode;
     }>) {
+
     return <>
         {children}  
-        {form}
     </>
 }
