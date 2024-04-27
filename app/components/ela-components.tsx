618d122cc3f38de2366c5e0f60c80e72c55df574
santher <erickhernandop@gmail.com>
2024-04-26 21:13:00 -0500

    Correccion de importacion del header

--- app/components/ela-components.tsx
+++ app/components/ela-components.tsx
@@ -1,6 +1,7 @@
 import Frame from "./frame/Frame";
 import Button from "./Button";
 import Responsiver from "./responsiver/Responsiver";
-import Header from "./header/Header";
+import Header from "./header/header";
+
 
 export {Frame,Button,Responsiver,Header}
