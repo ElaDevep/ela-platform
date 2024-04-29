d9be1db7b0d89d9244465510db0ee7e603052a70
santher <erickhernandop@gmail.com>
2024-04-29 15:17:26 -0500

    Error de renderiazado de form

--- app/(views)/(ingreso)/inicio_sesion/page.tsx
+++ app/(views)/(ingreso)/inicio_sesion/page.tsx
@@ -75,12 +75,15 @@ export default function LogIn() {
                     contain
                     />
                     <h2>Iniciar Sesión</h2>
-                    <Form className={styler.logIn_form} onSubmit={LogInHandler} styler={styler} >
+                    <form>
+                        <input type="text" />
+                    </form>
+                    {/* <Form className={styler.logIn_form} onSubmit={LogInHandler} styler={styler} >
                         <TextField name={"email"} required label="Correo"/>
                         <PasswordField name={"password"} required label="Contraseña"/>
                         <Submit>Ingresar</Submit>
-                        {/* <Link href={'/usuarios'} className={styler.asGuest_link}>Entrar como invitado</Link> */}
-                    </Form>
+                        {/* <Link href={'/usuarios'} className={styler.asGuest_link}>Entrar como invitado</Link>}
+                    </Form> */}
                     {Regret &&
                     <Link href={'/recuperacion_contrasena'} className={styler.forgotPassword_link}>
                         ¿Has olvidado tu contraseña?
